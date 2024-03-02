4634eb89b6018f3af13b1c29eb9e60002835b304
izabelabarbara <izabela.szymczak@outlook.com>
2024-03-02 21:29:12 +0100

    updated the scraper with an option to save results into a file

--- beautifulSoup_scraperTemplate.py
+++ beautifulSoup_scraperTemplate.py
@@ -2,20 +2,31 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 class Scraper:
+    results = []
+    
     def __init__(self, site):
         self.site = site
 
     def scrape(self):
+        """
+        scrape URLs from the site.
+        save the list of scraped URLs into a txt file.
+        """
         r = urllib.request.urlopen(self.site)
         html = r.read()
         parse = "html.parser"
         sp = BeautifulSoup(html, parse)
+        
         for tag in sp.find_all("a"):
             url = tag.get("href")
             if url is None:
                 continue
             if "html" in url:
-                print("\n" + url)
+                self.results.append(url)
+                
+        with open("beautifulSoup_scraperTemplate.txt", "a+") as f:
+            f.write(str(self.results))
 
+# Example URL
 news = "https://www.bbh.com/us/en/bbh-who-we-are/bbh-news.html"
 Scraper(news).scrape()
